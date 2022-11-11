74ab91af88f62735d394e9b5ac2e7f6bcb92e0db
ddiniz <ddiniz@student.42sp.org.br>
2022-11-11 13:59:15 -0300

    Feat: add boolean and MICROSECS.

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,13 +6,13 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 15:29:42 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/11/11 12:42:24 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/11/11 13:53:07 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
-# define MICROSECS 300
+# define MICROSECS 100
 # define TRUE 1
 # define FALSE 0
 
--- includes/minitalk_bonus.h
+++ includes/minitalk_bonus.h
@@ -6,13 +6,13 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 15:29:42 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/11/11 12:42:22 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/11/11 13:53:12 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
-# define MICROSECS 300
+# define MICROSECS 100
 # define TRUE 1
 # define FALSE 0
 
